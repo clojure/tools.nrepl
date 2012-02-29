d05b58ec20a416b404ca3842d8c04d08630e5868
Meikel Brandmeyer <mb@kotka.de>
2012-02-29 21:56:37 +0100

    Handle nil writing in bencode

--- src/main/clojure/clojure/tools/nrepl/bencode.clj
+++ src/main/clojure/clojure/tools/nrepl/bencode.clj
@@ -325,9 +325,7 @@
       (symbol? thing)  :named
       (keyword? thing) :named
       (map? thing)     :map
-      (or (coll? thing)
-          (.isArray (class thing)))
-      :list)))
+      (or (nil? thing) (coll? thing) (.isArray (class thing))) :list)))
 
 ;; The following methods should be pretty straight-forward.
 ;;
